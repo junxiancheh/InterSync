2b9a4459f2cb87564a7bc3a6a8ce0f5717ae58e4
junxiancheh <e1354852@u.nus.edu>
2025-06-23 00:45:59 +0800

    minor bug fixes

--- screens/Activitypage.js
+++ screens/Activitypage.js
@@ -1,7 +1,9 @@
 import StandNotification from '../features/StandNotification';
+import { ActivityLogger } from '../features/ActivityLogger';
 import { View, Text, ScrollView } from 'react-native';
 
 export default function ActivityPage() {
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
