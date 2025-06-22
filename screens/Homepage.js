080526a0e3994c6452cec196ead5883e8c6a4d19
junxiancheh <e1354852@u.nus.edu>
2025-06-22 16:42:29 +0800

    added new homepage layout

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,3 +1,34 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { DeskController } from '../features/DeskController';
+import DeskTypeSelector from "../features/DeskTypeSelector";
+
+export default function HomePage({ deskType, setDeskType }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>InterSync</Text>
+            <Text style={styles.subtitle}>Making Ergonomic Workspaces with Technology</Text>
+           <DeskTypeSelector deskType={deskType} setDeskType={setDeskType} />
+            <DeskController deskType={deskType} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f7fa',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#',
+    },
+    subtitle: {
+        fontSize: 18,
+        color: '#666',
+        marginBottom: 30,
+    },
+});
